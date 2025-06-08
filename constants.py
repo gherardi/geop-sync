98b09cf81a798b1e0750271587ee3ba9e5bba7d2
Victor Gherardi <gherardivictor@gmail.com>
2025-06-08 14:33:35 +0200

    add constants file

---
+++ constants.py
@@ -0,0 +1,6 @@
+# Constants
+DEFAULT_TIMEOUT = 10
+MAX_RETRIES = 3
+LECTURE_TAGS_TO_REMOVE = ["<br>", "[PRIMA LEZIONE]", "[ESAME]"]
+GOOGLE_SCOPES = ['https://www.googleapis.com/auth/calendar']
+TIMEZONE = "Europe/Rome"
